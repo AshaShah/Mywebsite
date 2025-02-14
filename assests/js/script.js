ac3a3139a3cf1c57f8cff9dff5d90e9ab7691aba
Asha Shah <shahaasha99@gmail.com>
2025-02-14 17:41:02 -0600

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -98,15 +98,15 @@ document.onkeydown = function(e) {
 }
 
 //Start of Tawk.to Script-->
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/67a85f0e825083258e12620e/1ijkr6k77';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+//var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+//(function(){
+//var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+//s1.async=true;
+//s1.src='https://embed.tawk.to/67a85f0e825083258e12620e/1ijkr6k77';
+//s1.charset='UTF-8';
+//s1.setAttribute('crossorigin','*');
+//s0.parentNode.insertBefore(s1,s0);
+//})();
 //End of Tawk.to Script-->
 
 
